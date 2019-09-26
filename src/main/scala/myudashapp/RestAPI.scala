f80882d29b757f2f3dfd13e0fedcbf537b8f938c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-09-26 13:13:05 +0200

    Explicit companion for a Team working.

--- src/main/scala/myudashapp/RestAPI.scala
+++ src/main/scala/myudashapp/RestAPI.scala
@@ -1,14 +1,23 @@
 package myudashapp
 
 import com.avsystem.commons.serialization.GenCodec
-import io.udash.rest.{DefaultRestImplicits, GET, RestApiCompanion, RestDataCompanion}
+import io.udash.rest.{CodecWithStructure, DefaultRestImplicits, GET, RestApiCompanion, RestDataCompanion}
 
 import scala.concurrent.Future
-
 import JavaPersonFakeCompanion._
+import com.avsystem.commons.meta.MacroInstances
+import io.udash.rest.openapi.{RestSchema, RestStructure}
 
 case class Team(leader: JavaPerson)
-object Team extends RestDataCompanion[Team]
+
+object Team {
+  type T = Team
+  implicit val javaPersonCodec = JavaPersonFakeCompanion.javaPersonCodec
+  implicit val instances: MacroInstances[DefaultRestImplicits, CodecWithStructure[T]] = implicitly[MacroInstances[DefaultRestImplicits, CodecWithStructure[T]]]
+  implicit lazy val codec: GenCodec[T] = instances(DefaultRestImplicits, this).codec
+  implicit lazy val restStructure: RestStructure[T] = instances(DefaultRestImplicits, this).structure
+  implicit lazy val restSchema: RestSchema[T] = RestSchema.lazySchema(restStructure.standaloneSchema)
+}
 
 trait EnhancedRestImplicits extends DefaultRestImplicits {
   implicit val javaPersonCodec: GenCodec[JavaPerson] = JavaPersonFakeCompanion.javaPersonCodec
