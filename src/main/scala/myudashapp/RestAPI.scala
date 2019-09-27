76e39acb1312e953572b9524298b7c8a664990e3
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2019-09-27 08:24:06 +0200

    Generic EnhancedRestApiCompanion using HasGenCodecWithDeps.

--- src/main/scala/myudashapp/RestAPI.scala
+++ src/main/scala/myudashapp/RestAPI.scala
@@ -1,32 +1,29 @@
 package myudashapp
 
-import com.avsystem.commons.serialization.GenCodec
+import com.avsystem.commons.serialization.{GenCodec, HasGenCodecWithDeps}
 import io.udash.rest.{CodecWithStructure, DefaultRestImplicits, GET, RestApiCompanion, RestDataCompanion}
 
 import scala.concurrent.Future
-import JavaPersonFakeCompanion._
 import com.avsystem.commons.meta.MacroInstances
 import io.udash.rest.openapi.{RestSchema, RestStructure}
-
-case class Team(leader: JavaPerson)
-
-abstract class EnhancedRestDataCompanion {
-  type T = Team
-  implicit val javaPersonCodec = JavaPersonFakeCompanion.javaPersonCodec
-  //implicit val instances: MacroInstances[DefaultRestImplicits, CodecWithStructure[T]] = MacroInstances.materialize[DefaultRestImplicits, CodecWithStructure[T]]
-  implicit val instances: MacroInstances[DefaultRestImplicits, CodecWithStructure[T]] = implicitly[MacroInstances[DefaultRestImplicits, CodecWithStructure[T]]]
-  implicit lazy val codec: GenCodec[T] = instances(DefaultRestImplicits, this).codec
-  implicit lazy val restStructure: RestStructure[T] = instances(DefaultRestImplicits, this).structure
-  implicit lazy val restSchema: RestSchema[T] = RestSchema.lazySchema(restStructure.standaloneSchema)
-}
-
-object Team extends EnhancedRestDataCompanion
 
 trait EnhancedRestImplicits extends DefaultRestImplicits {
   implicit val javaPersonCodec: GenCodec[JavaPerson] = JavaPersonFakeCompanion.javaPersonCodec
 }
 
 object EnhancedRestImplicits extends EnhancedRestImplicits
+
+abstract class EnhancedRestApiCompanion[T](
+  implicit macroCodec: MacroInstances[EnhancedRestImplicits.type, () => GenCodec[T]]
+) extends HasGenCodecWithDeps[EnhancedRestImplicits.type, T] {
+  implicit val instances: MacroInstances[DefaultRestImplicits, CodecWithStructure[T]] = implicitly[MacroInstances[DefaultRestImplicits, CodecWithStructure[T]]]
+  implicit lazy val restStructure: RestStructure[T] = instances(DefaultRestImplicits, this).structure
+  implicit lazy val restSchema: RestSchema[T] = RestSchema.lazySchema(restStructure.standaloneSchema)
+}
+
+case class Team(leader: JavaPerson)
+
+object Team extends EnhancedRestApiCompanion[Team]
 
 trait RestAPI {
   @GET
